f998f24aabc7af5271d8376c16b6a197e449b9a8
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-23 23:44:27 -0400

    updated nav

--- App.js
+++ App.js
@@ -1,56 +1,9 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { UserScreen } from './src/Modal/UserScreen';
+import { VendorScreen } from './src/Modal/VendorScreen';
+import { ProductScreen } from './src/Modal/ProductScreen';
+import { PurchaseRequestScreen } from './src/Modal/PurchaseRequestScreen';
+import { LineItemScreen } from './src/Modal/LineItemScreen';
 import { createMaterialTopTabNavigator } from 'react-navigation';
-
-class UserScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Users!</Text>
-      </View>
-    );
-  }
-}
-
-class VendorScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Vendors!</Text>
-      </View>
-    );
-  }
-}
-
-class ProductScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Products!</Text>
-      </View>
-    );
-  }
-}
-
-class PurchaseRequestScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>PurchaseRequests!</Text>
-      </View>
-    );
-  }
-}
-
-class LineItemScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>LineItems!</Text>
-      </View>
-    );
-  }
-}
 
 export default createMaterialTopTabNavigator(
   {
