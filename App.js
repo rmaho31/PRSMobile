9bb5cc37670b8c137e2dac59d0871389728d75b9
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-25 23:29:21 -0400

    changed routing

--- App.js
+++ App.js
@@ -1,26 +1,2 @@
-import { UserScreen } from './src/Modal/UserScreen';
-import { VendorScreen } from './src/Modal/VendorScreen';
-import { ProductScreen } from './src/Modal/ProductScreen';
-import { PurchaseRequestScreen } from './src/Modal/PurchaseRequestScreen';
-import { LineItemScreen } from './src/Modal/LineItemScreen';
-import { createMaterialTopTabNavigator } from 'react-navigation';
-
-export default createMaterialTopTabNavigator(
-  {
-    Users: UserScreen,
-    Vendors: VendorScreen,
-    Products: ProductScreen,
-    PurchaseRequests: PurchaseRequestScreen,
-    LineItems: LineItemScreen
-  },
-  {
-    tabBarOptions: {
-      labelStyle: {
-        fontSize: 8,
-        color: "#fdcb6e"
-      },
-      style: {
-        backgroundColor: 'black'
-      },
-    }
-  });
+import App from "./src/index.js";
+export default App;
